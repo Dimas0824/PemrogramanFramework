4b9e72d6ba787fbf208246201642a9e9e55288ee
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-02-22 18:41:58 +0700

    feat: add Halaman Toko page with dynamic routing and category display

--- nextJS/src/pages/shop/[[...slug]].tsx
+++ nextJS/src/pages/shop/[[...slug]].tsx
@@ -10,8 +10,11 @@ const HalamanToko = () => {
                 Halaman Toko
             </h1>
             <p>
-                Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1]}`}
+                Toko: {`${query.slug && query.slug[0] + "-" + query.slug[1] || ""}`}
                 {/* Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug} */}
+            </p>
+            <p>
+                Kategori: {query.slug && query.slug[0] ? query.slug[0] : "Semua Kategori"}
             </p>
         </div>
     )
