a9cc00b6a58d5e6c06b4aa9b1cc565776ec12d85
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-02-23 21:51:52 +0700

    feat: add 404 error page and corresponding styles for better user experience

---
+++ nextJS/src/pages/404.tsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/404.module.scss";
+
+const Custom404 = () => {
+    return (
+        <div className={styles.error}>
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+        </div>
+    );
+};
+
+export default Custom404;
--- nextJS/src/pages/index.tsx
+++ nextJS/src/pages/index.tsx
@@ -5,6 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className={inter.className}>
+      <head>
+        <title>Praktikum Next.js Pages Router</title>
+      </head>
       <h1>Praktikum Next.js Pages Router</h1>
       <p>Mahasiswa D4 Pengembangan Web</p>
     </div>
