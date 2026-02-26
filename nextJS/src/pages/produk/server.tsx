cbc03933553b1a10c0b6fc1e53ded73a2b9aad4e
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-02-26 14:16:31 +0700

    feat: enhance HalamanProdukServer with type definitions and server-side data fetching

--- nextJS/src/pages/produk/server.tsx
+++ nextJS/src/pages/produk/server.tsx
@@ -1,12 +1,36 @@
-import TampilanProduk from "@/views/product";
+import TampilanProduk, { ProductType } from "@/views/product";
+import { GetServerSideProps } from "next";
 
-const HalamanProdukServer = () => {
+type HalamanProdukServerProps = {
+  products: ProductType[];
+};
+
+const HalamanProdukServer = ({ products }: HalamanProdukServerProps) => {
   return (
     <div>
       <h1>Halaman Produk Server</h1>
-      <TampilanProduk products={[]} isLoading />
+      <TampilanProduk products={products} />
     </div>
   )
 }
 
 export default HalamanProdukServer;
+
+export const getServerSideProps: GetServerSideProps<HalamanProdukServerProps> = async (
+) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/produk");
+    const response = await res.json();
+    return {
+      props: {
+        products: response.data || [],
+      },
+    };
+  } catch {
+    return {
+      props: {
+        products: [],
+      },
+    }
+  }
+};
