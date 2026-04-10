5d2e5d92ee7cede430d81b9648173900bccaaf21
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-04-10 07:00:49 +0700

    feat: add Lighthouse summary JSON and integrate dynamic Google Analytics component

--- nextJS/src/pages/_app.tsx
+++ nextJS/src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
+import dynamic from "next/dynamic";
 import type { AppProps } from "next/app";
 import AppShell from "@/components/layouts/AppShell";
 import { SessionProvider } from "next-auth/react";
 import { appSansFont } from "@/lib/fonts";
-import GoogleAnalytics from "@/components/analytics/GoogleAnalytics";
+
+const GoogleAnalytics = dynamic(() => import("@/components/analytics/GoogleAnalytics"), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
