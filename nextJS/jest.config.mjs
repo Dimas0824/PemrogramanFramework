765ca84a39192f6de0a93a3e82751524fd227f1d
Dimas0824 <Dimas0824@users.noreply.github.com>
2026-04-10 08:02:58 +0700

    feat: update Jest configuration and enhance package dependencies for improved testing

--- nextJS/jest.config.mjs
+++ nextJS/jest.config.mjs
@@ -1,12 +1,24 @@
-import nextJest from 'next/jest.js';
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: "./",
 });
 
 const config = {
-    coverageProvider: 'v8',
-    testEnvironment: 'jsdom',
-}
+  testEnvironment: "jsdom",
+  coverageProvider: "v8",
+  modulePaths: ["<rootDir>/src/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.spec.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/__test__/**",
+    "!src/pages/api/**",
+    "!src/views/**",
+    "!src/types/**",
+  ],
+};
 
 export default createJestConfig(config);
